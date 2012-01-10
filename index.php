5052878f6fabcfff8b00f0e3e775622a2dd2734e
Mark Learst <learst@me.com>
2012-01-09 19:45:46 -0500

    .

---
+++ index.php
@@ -0,0 +1,181 @@
+<!doctype html>
+
+<!--[if lt IE 7 ]> <html class="ie ie6 no-js" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 no-js" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 no-js" lang="en"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 no-js" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!-- the "no-js" class is for Modernizr. -->
+
+<head id="www-sitename-com" data-template-set="html5-reset">
+
+	<meta charset="utf-8">
+	
+	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<title>Revitalization &amp; Business Initiative</title>
+
+	<link rel="shortcut icon" href="_/img/favicon.ico">
+	
+	<!-- CSS: screen, mobile & print are all in the same file -->
+	<link rel="stylesheet" href="_/css/style.css">
+	
+	<!-- all our JS is at the bottom of the page, except for Modernizr. -->
+	<script src="_/js/modernizr-2.0.6.min.js"></script>
+
+</head>
+
+<body>
+
+<div class="wrapper"><!-- not needed? up to you: http://camendesign.com/code/developpeurs_sans_frontieres -->
+	<div class="header-wrapper">
+		<header>
+			<div class="header-inner center min-max clearfix">
+
+				<h1><a href="/">Revitalization &amp; Business Initiative</a></h1>
+		
+				<nav class="main-nav">
+			
+					<ol>
+						<li class="active"><a href="">Initiative</a></li>
+						<li class=""><a href="">Conference</a>
+						<!-- <ul class="main-nav-secondary">
+													<li class=""><a href="">Agenda</a></li>
+													<li class="active"><a href="">IdeaLab</a></li>
+													<li class=""><a href="">Panels</a></li>
+													<li class=""><a href="">Keynote Speakers</a></li>
+												</ul> -->
+						</li>
+						<li class=""><a href="">Detroit Impact</a></li>
+						<li class=""><a href="https://www.eventbrite.com/event/1290514965?utm_source=eb_email&utm_medium=email&utm_campaign=new_eventv2&utm_term=eventurl_text">Register</a></li>
+					</ol>
+		
+				</nav>
+				
+				<ul class="social">
+					<li class="facebook"><a href="http://www.facebook.com/rossrnb" target="_blank">Facebook</a></li>
+					<li class="twitter"><a href="http://www.twitter.com/rossrnb" target="_blank">Twitter</a></li>
+				</ul>
+			
+				<form action="" method="get">
+			
+					<fieldset>
+				
+						<legend>
+							<span class="legendWord">sub&middot;scrip&middot;tion</span>
+							<span class="legendSubword">[suhb-skrip-shuhn]</span>
+						</legend>
+				
+						<label for="email">1. Get R&amp;B Conference details 2. see insider videos and photo gallaries 3. follow what&rsquo;s happening in Detroit.</label>
+						<input type="text" name="email" placeholder="Email Please">
+						<input type="submit" name="sendemail" value="IM IN!">
+				
+					</fieldset>
+				
+				</form>
+
+			</div>
+			
+			<div class="message clearfix">
+				<h2>We’re changing<span>the conversation</span></h2>
+				<p>Educating and engaging students in business issues shaping Detroit</p>
+			</div>
+			<!-- <div class="billboard-gallery">
+				<ul class="billboard-gallery-inner">
+					<li><img src="_/img/1.jpg" alt="2"></li>
+					<li><img src="_/img/2.jpg" alt="2"></li>
+					<li><img src="_/img/2.jpg" alt="2"></li>
+				</ul>
+			</div> -->
+			
+			
+			<div class="billboard"></div>
+		
+		</header>
+	</div>
+	
+	<div class="content-wrapper clearfix">
+		
+		<section class="center min-max">
+			
+			<ul class="col-three">
+				<li class="pillar">
+					<h3 class="ross">Michigan Ross School of Business</h3>
+					<h4>Who we are</h4>
+					<p>Launched by Ross School of Business MBAs in 2010, R&amp;B connects University of Michigan students with Detroit&rsquo;s evolving business landscape. R&amp;B promotes Detroit&rsquo;s assets and encourages students to explore opportunities in the Motor City.</p>
+				</li>
+				<li class="pillar">
+					<h3 class="oldenglish">Detroit</h3>
+					<h4>What we do</h4>
+					<p>R&amp;B hosts an annual capstone conference examining key business initiatives in Detroit. We also organize excursions and plan events to highlight the city&rsquo;s career opportunities, rich culture, and quality of life.</p>
+				</li>
+				<li>
+					<h3 class="cityscape">City</h3>
+					<h4>Where we are going</h4>
+					<p>We strive for impact by contributing to Detroit organizations through student-run consulting projects and motivating UM students to choose Detroit as a career and lifestyle destination. R&amp;B engages students with the important issues shaping the city&rsquo;s future.</p>
+				</li>
+			</ul>
+			
+		</section>
+		
+	</div>
+	
+	<footer>
+		<div class="footer-wrapper-info">
+			<ul class="center min-max clearfix">
+				<li class="info-first">40<span>Students working on projects with</span></li>
+				<li class="info-second">8<span>Detroit Businesses</span></li>
+				<li class="info-third">800<span>Attendees over two days</span></li>
+				<li class="noarrow info-fourth">10<span>Handpicked Detroit change-makers presenting at Idealab</span></li>
+			</ul>
+		</div>
+		<div class="footer-wrapper-thankyou clearfix">
+			<div class="center min-max">
+				<h5>Thank you for your support</h5>
+				<ul class="center min-max">
+					<li class="gm">GM</li>
+					<li class="det">DTE Energy</li>
+					<li class="pwc">Price Waterhouse Coppers</li>
+					<li class="netimpact">Net Impact Michigan</li>
+					<li class="del">Deloitte.</li>
+					<li class="nelsm">New Economy Initiative for Southest Michigan</li>
+				</ul>
+			</div>
+		</div>
+		
+		<p class="center min-max">University of Michigan Revitalization &amp; Business Club<a href="">Reach out to us &rarr;</a></p>
+		
+	</footer>
+
+</div>
+
+<!-- here comes the javascript -->
+
+<!-- Grab Google CDN's jQuery. fall back to local if necessary -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script>window.jQuery || document.write("<script src='_/js/jquery-1.7.1.min.js'>\x3C/script>")</script>
+
+<!-- this is where we put our custom functions -->
+<script src="_/js/functions.js"></script>
+
+<!-- Asynchronous google analytics; this is the official snippet.
+	 Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.
+	 
+<script>
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-XXXXXX-XX']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+-->
+  
+</body>
+</html>
